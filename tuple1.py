8355d1527092c7237f56e473ce6b317636667e3c
niharff0321-debug <niharff0321@gmail.com>
2025-12-14 20:39:47 +0530

    Add files via upload

---
+++ tuple1.py
@@ -0,0 +1,13 @@
+tuple2=(False ,45,4.5,"alphabet")
+print(tuple2)
+tuple543879=(34,56,76,34,56,34,67,4)
+print(tuple543879)
+tuple543879=tuple543879+(8,)
+print(tuple543879)
+tuple_uirt=(564,87,345,444,444,888,888)
+print(tuple_uirt.count(888))
+tuple_gueyf=(654337,64585,5437665437965,7,6,867,567534,395687568984567994750047598)
+slice1=tuple_gueyf[3:8]
+print(slice1)
+slice1=tuple_gueyf[:8]
+print(slice1)
